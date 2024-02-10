d28064264678d7e8b894990243c805f7abf15a48
alpakaFred <preis.coding@mailbox.org>
2024-02-10 17:13:20 +0100

    Integrated Cross-Origin Resource Sharing

--- frontend-vue/src/views/ImportView.vue
+++ frontend-vue/src/views/ImportView.vue
@@ -2,12 +2,26 @@
     <div class="d-flex flex-column align-items-center">
         <h1>Import</h1>
         <h4>Import your CSV-File here</h4>
+        {{ data }}
     </div>
 </template>
 
 <script>
+import { ref } from 'vue'
 export default {
-    name: 'ImportView'
-    
+    name: 'ImportView',
+    setup() {
+
+        // API Call (Files)
+        const data = ref(null)
+        const fetchData = async () => { 
+            const response = await fetch('http://127.0.0.1:8000/api/files/')
+            const responseData = await response.json()
+            data.value = responseData
+        }
+        fetchData()
+        return { data }
+    }
+   
 }
 </script>
