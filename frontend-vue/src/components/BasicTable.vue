cc89042ff85a72e787deecfaabeca468795ff74e
alpakaFred <preis.coding@mailbox.org>
2024-02-11 15:35:07 +0100

    BasicTable Component build

---
+++ frontend-vue/src/components/BasicTable.vue
@@ -0,0 +1,30 @@
+<template>
+<table class="table">
+    <thead>
+        <tr>
+            <th class="bg-secondary text-primary" v-for="key in Object.keys(jsonData[0])" :key="key">
+                {{ key }}
+            </th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr v-for="entry in jsonData" :key="entry.id">
+            <td v-for="value in Object.values(entry)" :key="value">
+                {{ value }}
+            </td>
+        </tr>
+    </tbody>
+</table> 
+</template>
+
+<script setup>
+
+import { onMounted } from 'vue';
+
+const props = defineProps({
+  jsonData: Object
+})
+
+
+
+</script>
--- frontend-vue/src/views/ImportView.vue
+++ frontend-vue/src/views/ImportView.vue
@@ -2,13 +2,14 @@
     <div class="d-flex flex-column align-items-center">
         <h1>Import</h1>
         <h4>Import your CSV-File here</h4>
-        {{ data }}
+        <BasicTable v-if="data" :jsonData="data" />
     </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import { getData } from '../composables/API.js'
+import BasicTable from '../components/BasicTable.vue'
 
 const data = ref(null)
 
