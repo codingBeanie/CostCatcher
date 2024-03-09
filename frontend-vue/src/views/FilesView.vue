30295f68b03ab711eaff9d6a1f95eda90d5b69c4
alpakaFred <preis.coding@mailbox.org>
2024-03-09 16:06:31 +0100

    upload- table

--- frontend-vue/src/views/FilesView.vue
+++ frontend-vue/src/views/FilesView.vue
@@ -2,13 +2,18 @@
     <!--Titles-->
     <div>
         <h1 class="mb-3 text-h3 font-weight-bold">Uploads</h1>
-        <p class="mb-4 text-h7">Manage your uploaded files here.</p>
+        <p class="mb-4 text-h7">Manage your uploaded files here. Delete all corresponding transactions of a previous file upload.</p>
     </div>
     <v-divider class="mb-8"></v-divider>
 
     <!--File-Table-->
     <div>
         <v-data-table :items="data" :headers="headers">
+            <!--Date-->
+            <template v-slot:item.fileDate="{ item }">
+                {{ new Date(item.fileDate).toLocaleString() }}
+            </template>
+
             <template v-slot:item.action="{ item }">
                 <v-btn density="compact" icon="mdi-delete" color="" @click="deleteItem(item)">
                 </v-btn>
@@ -24,8 +29,8 @@ import { API } from '../composables/API.js'
 const data = ref([])
 const headers = [
     { title: 'File Name', value: 'fileName' },
-    { text: 'File Date', value: 'fileDate' },
-    { text: 'Actions', value: 'action'}
+    { title: 'Upload Date', value: 'fileDate' },
+    { title: 'Actions', value: 'action', align: 'center'}
 ]
 
 // Methods
