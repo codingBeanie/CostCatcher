076daffa2e1c84e7f8d169ca52b26705108c86ba
alpakaFred <preis.coding@mailbox.org>
2024-05-13 05:47:16 +0200

    added some styling

--- frontend-vue/src/composables/Tutorials.js
+++ frontend-vue/src/composables/Tutorials.js
@@ -13,11 +13,11 @@ export const Tutorials = [
           },
           {
             title: 'Change the import settings',
-            text: 'You will most likely have to adjust the import settings to match the format of the csv file. This includes the date format, the column separator, and the column mapping. You can do this in the tab `CSV SETTINGS` in the settings menu.'
+            text: 'You will most likely have to adjust the import settings to match the format of the csv file. This includes the date format, the column separator, and the column mapping. You can do this in the tab <span class="text-button">CSV-SETTINGS</span> in the <span class="text-button">SETTINGS</span> menu.'
           },
           {
             title: 'Review the imported data',
-            text: 'Check if the imported data looks correct. If not, try to change the `CSV SETTINGS` accordingly.'
+            text: 'Check if the imported data looks correct. If not, try to change the <span class="text-button">CSV-SETTINGS</span> accordingly.'
           },
         ]
     },
